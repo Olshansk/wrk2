018f49c12bbec933ddbe3b7b3084f51ba75b59c9
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-09-19 17:23:40 -0700

    Fix Homebrew formula for Apple Silicon build
    - Remove SHA256 (not needed for HEAD builds)
    - Fix bytecode generation issue that causes linker errors
    - Properly compile LuaJIT bytecode as C source first
    - Works with existing Makefile that already has correct OpenSSL paths

--- wrk2.rb
+++ wrk2.rb
@@ -1,7 +1,6 @@
 class Wrk2 < Formula
-  desc "HTTP benchmarking tool"
+  desc "Constant throughput, correct latency recording variant of wrk"
   homepage "https://github.com/olshansk/wrk2"
-  sha256 "e6937d5963fb6285598a3ca698cd3afe1f7f780c"
   head "https://github.com/olshansk/wrk2.git"
 
   depends_on "openssl"
@@ -10,12 +9,30 @@ class Wrk2 < Formula
     # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
     # is not set then it's forced to 10.4, which breaks compile on Mojave.
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+    # Build LuaJIT first
+    system "make", "clean"
+    system "make", "deps/luajit/src/libluajit.a"
+
+    # Generate bytecode properly
+    cd "deps/luajit/src" do
+      system "./luajit", "-b", "-n", "wrk", "-t", "c",
+             buildpath/"src/wrk.lua", buildpath/"obj/bytecode.c"
+    end
+
+    # Compile bytecode
+    mkdir "obj" unless File.exist?("obj")
+    system ENV.cc, "-c", "-o", "obj/bytecode.o", "obj/bytecode.c"
+
+    # Build wrk2
     system "make"
+
+    # Rename and install
     mv "wrk", "wrk2"
     bin.install "wrk2"
   end
 
   test do
-    system *%W[#{bin}/wrk2 -r 5 -c 1 -t 1 -d 1 https://example.com/]
+    system "#{bin}/wrk2", "--version"
   end
 end
